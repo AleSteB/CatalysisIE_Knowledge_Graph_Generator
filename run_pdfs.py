d05bd53456e263d357508a6d3733a019fedbcaf5
AlexB <55703624+AleSteB@users.noreply.github.com>
2024-07-31 17:23:49 +0200

    Update run_pdfs.py

--- run_pdfs.py
+++ run_pdfs.py
@@ -9,7 +9,8 @@ Created on Sat Sep 23 14:07:50 2023
    """
 from text_mining import load_classes_chebi, delete_files_in_directory, run_text_mining, add_publication
 from preprocess_onto import *
-from onto_extension_new import preprocess_classes,create_classes_onto 
+from onto_extension_new import preprocess_classes,create_classes_onto
+#from onto_extension import preprocess_classes,create_classes_onto
 from txt_extract import get_abstract, get_metadata
 from CatalysisIE.model import *
 from CatalysisIE.utils import *
